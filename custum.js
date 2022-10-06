66395b14762f91e8ce7e47e0ad787c64a2fcfcb7
kallenstagray <113639898+kallenstagray@users.noreply.github.com>
2022-10-06 17:59:54 -0400

    bot

--- custum.js
+++ custum.js
@@ -1,10 +1,8 @@
-alert("hello");alert("this");alert("is");alert("the");alert("official");alert("shrek");alert("website");
+$(()=>{
+  $('.odd').css('color','red')  
 
-($document).onload = function () {
-alert(" hehe haha!");
 $('#blue-header-button').click(()=>{
-$('.odd').css('color','red')
-
+    $('.odd').css('color','blue')
 })
 
-};
+});
