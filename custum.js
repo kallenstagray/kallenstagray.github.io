9968363ccccf7a0c379474318eb3c002c91bf5da
kallenstagray <113639898+kallenstagray@users.noreply.github.com>
2022-10-06 17:37:19 -0400

    bgug

---
+++ custum.js
@@ -0,0 +1,10 @@
+alert("hello");alert("this");alert("is");alert("the");alert("official");alert("shrek");alert("website");
+
+($document).onload = function () {
+alert(" hehe haha!");
+$('#blue-header-button').click(()=>{
+$('.odd').css('color','red')
+
+})
+
+};
