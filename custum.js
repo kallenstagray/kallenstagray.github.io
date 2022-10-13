5fcd6f9067a82e26b0e6f036c5baab622549b5f8
kallenstagray <113639898+kallenstagray@users.noreply.github.com>
2022-10-13 17:38:06 -0400

    hehehaha

--- custum.js
+++ custum.js
@@ -1,8 +1,40 @@
 $(()=>{
   $('.odd').css('color','red')  
 
-$('#blue-header-button').click(()=>{
-    $('.odd').css('color','blue')
+$('#white-header-button').click(()=>{
+    $('.odd').css('color','white')
+  })
+    $('#black-header-button').click(()=>{
+      $('.odd').css('color','black')
+     
+      $('#green-header-button').click(()=>{
+        $('.odd').css('color','green')
+  })
+  $('#yellow-header-button').click(()=>{
+    $('.odd').css('color','yellow')
+})
+$('#red-header-button').click(()=>{
+  $('.odd').css('color','red')
+})
+$('#plum-header-button').click(()=>{
+  $('.odd').css('color','plum')
+})
+$('#Amaranth-header-button').click(()=>{
+  $('.odd').css('color','Amaranth')
+})
+$('#Atrovirens-header-button').click(()=>{
+  $('.odd').css('color','Atrovirens')
+})
+$('#Vermilion-header-button').click(()=>{
+  $('.odd').css('color','Vermilion')
+
 })
 
-});
+
+
+
+
+  
+})
+
+})
